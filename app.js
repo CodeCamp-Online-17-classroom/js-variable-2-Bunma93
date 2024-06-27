215e94459b71c7e3774112e14a7b415fb0c37515
Bunma93 <bunma93@gmail.com>
2024-06-27 08:55:14 +0700

    Js-variable2

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // write code here
+let money = 500;
+let fathername = "นายไพสัน บุญมา";
+let mothername = "นางฉัฐสุนันท์ บุญมา";
+let Address = "25/2 หมู่ 4 แขวงทวีวัฒนา เขตทวีวัฒนา กรุงเทพฯ 10170";
+let univeseage = 2.7e10;
